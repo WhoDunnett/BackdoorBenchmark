Changes to FST to correct inner product calculation.

defense/fst.py
@@ -264,13 +264,13 @@ class FST(defense):
                 # Loss 1 is the inner product of the original weights and the new weights
                 inner_product = torch.tensor(0.0).to(args.device)
                 for i in range(len(original_weights)):
-                    inner_product += torch.sum(original_weights[i] * new_layer[i].weight.data)
+                    inner_product += torch.sum(original_weights[i] * new_layer[i].weight)
                 
                 # Loss 2 is the cross entropy loss
                 ce_loss = critierion(outputs, labels)
                 
                 # Total loss and backpropagation
-                loss = ce_loss + (args.alpha * inner_product)
+                loss = (args.alpha * inner_product) + ce_loss
                 loss.backward()
                 
                 optimizer.step()

defense/pbe.py
@@ -238,6 +238,7 @@ class PBE(defense):
         lr = args.lr
         
         criterion = nn.CrossEntropyLoss()
+        optim_model = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
         
         for batch in train_loader:
             
@@ -246,7 +247,6 @@ class PBE(defense):
             # NOTE: We use an alternative implementation of PGD attack here as the original method is unstable
             # and the model outputs become to be NaN after a few iterations.
             adversarial_inputs = pgd_attack(model, inputs, labels, args.device, eps=eps, alpha=alpha, iters=maxiter)           
-            optim_model = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
             
             model.train()
             for param in model.parameters():